1d220b991758a8cff604703be5a1423c409574a3
HumbertDominique <dominique@cadej.org>
2025-04-10 12:19:08 +0200

    Finalized the measurement script
    - added option for imaging parameters
    - still need to check arguments

--- measurment.py
+++ measurment.py
@@ -1,43 +1,49 @@
 from pypylon import pylon
-import keyboard
 import time
-import os
-import re
 
-
-def get_next_image_index(filepath = "data",prefix="image_", extension=".png"):
-    # List files and extract numbers
-    existing_files = [f for f in os.listdir(filepath) if f.startswith(prefix) and f.endswith(extension)]   
-    indices = []
-
-    for f in existing_files:
-        match = re.search(rf"{prefix}(\d+){extension}", f)
-        if match:
-            indices.append(int(match.group(1)))
-
-    return max(indices, default=0) + 1
-
-def main(fileprefix="image_",filepath = "/data/",ExpTime = 5000.0):
+def main(fileprefix="image_",filepath = "/data/",ExpTime = 5000.0,bitdepth=12):
+    # TODO: docstring
+    # TODO: arg checks
+    RUN = True
     # Initialize camera
     camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
     print("Using device:", camera.GetDeviceInfo().GetModelName())
     camera.Open()
+    camera.ExposureMode.SetValue("Timed")
+    camera.ExposureAuto.SetValue("Off")
     camera.ExposureTime.Value = ExpTime
-    print("Press 'space' to take a picture. Press 'esc' to exit.")
+    camera.GainAuto.Value = "Off"
+    camera.Gain.Value = 1.
 
-    image_index = get_next_image_index(prefix=fileprefix)
+    if bitdepth==8:
+        camera.PixelFormat.SetValue("Mono8")
+    elif bitdepth==12:
+        camera.PixelFormat.SetValue("Mono12")
+    print("sensor format: {camera.PixelFormat.Value}")
+
+    main_menu_string = "\n1) Press 'enter' to take a picture, 2)\nInput 'e' to adjust exposure time\n3) Input 'bpp' to adjust pixel bit depth,\n4) Input 'q' to exit."
+    image_index = 1
     img = pylon.PylonImage()
     tlf = pylon.TlFactory.GetInstance()
     imageWindow = pylon.PylonImageWindow()
     imageWindow.Create(1)
-    while True:
-        if keyboard.is_pressed('space'):
+    print(main_menu_string)
+    while RUN ==True:
+        user_input = input()
+        if user_input=='':
             print("Capturing image...")
             camera.StartGrabbingMax(1)
-
             while camera.IsGrabbing(): 
                 with camera.RetrieveResult(2000) as result:
                     img.AttachGrabResultBuffer(result)
+                    
+
+                    max_value = result.Array.max()
+                    print(f"Image pixel max value: {max_value}")
+                    if max_value >= 2**bitdepth-5:
+                        print("Image is overexposed. Decrease exposure time.")
+                    imageWindow.SetImage(result)
+                    imageWindow.Show()
                     # quality (100 -> best quality, 0 -> poor quality).
                     ipo = pylon.ImagePersistenceOptions()
                     quality = 100
@@ -45,20 +51,42 @@ def main(fileprefix="image_",filepath = "/data/",ExpTime = 5000.0):
                     filename = filepath+'/'+fileprefix+"_%d.jpeg" % image_index
                     img.Save(pylon.ImageFileFormat_Jpeg, filename, ipo)
                     image_index +=1
-                    imageWindow.SetImage(result)
-                    imageWindow.Show()
-                    print("Press 'space' to take a picture. Press 'esc' to exit.")
+                    print(main_menu_string)
+        elif user_input=="e":
+            print(f"curent exposure time:{ExpTime:.3e} us")
+            print("Input new exposure time in microseconds: 16.0 <= Exposure time <= 10e7")
+            ExpTime = input()
+            if ExpTime > 16.0 or ExpTime < 1e7:
+                camera.ExposureTime.Value = float(ExpTime)
+                print(f"New exposure time:{ExpTime:.3e} us")
+            else:
+                print(f"Invalid input")
 
-        elif keyboard.is_pressed('esc'):
+            print(main_menu_string)
+        elif user_input=="bpp":
+            print(f"curent pixel bit depth:{ExpTime:.3e} us")
+            print("Input new pixel bit depth: bpp = {8, 12}")
+            bitdepth = int(input())
+            if bitdepth==8:
+                camera.PixelFormat.SetValue("Mono8")
+            elif bitdepth==12:
+                camera.PixelFormat.SetValue("Mono12")
+            print("sensor format: {camera.PixelFormat.Value}")
+            print(main_menu_string)
+        elif user_input=='q':
             print("Exiting...")
-            break
-
+            RUN = False
         time.sleep(0.1)
     imageWindow.Close()
     camera.Close()
+    print("Program terminated")
  
-fileprefix = "test_"
+fileprefix = "calib_3mm_"
 filepath = "data"
-ExpTime = 100000.0 # [us]
+bitdepth = 12
+ExpTime = 150.0 # [us]
+print(filepath,'/', fileprefix)
+print(f"Exposure time: {ExpTime:.3e} us") 
+
 if __name__ == "__main__":
-    main(fileprefix, filepath, ExpTime)
+    main(fileprefix, filepath, ExpTime, bitdepth)
